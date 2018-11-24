da836a339d4986844e05b9239077b0891eb91691
Shubham Yadav <30603750+yshubham@users.noreply.github.com>
2018-11-24 07:04:10 +0530

    StopnWait
    ComputerNetworks

---
+++ stopnwait.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int  main()
+{
+    int i,j,noframes,x,x1=10,x2;
+    for(i=0;i<200;i++)
+    rand();
+    noframes=rand()/200;
+    i=1;
+    j=1;
+    noframes = noframes / 8;
+    printf("\n number of frames is %d",noframes);
+    
+    while(noframes>0)
+    {
+    printf("\nsending frame %d",i);
+    srand(x1++);
+    x = rand()%10;
+    if(x%2 == 0)
+    {
+    for (x2=1; x2<2; x2++)
+    {
+    printf("waiting for %d seconds\n", x2);
+    }
+    printf("\nsending frame %d",i);
+    srand(x1++);
+    x = rand()%10;}
+    printf("\nack for frame %d",j);
+    noframes-=1;
+    i++;
+    j++;
+    }
+    printf("\n end of stop and wait protocol");
+    return 0;
+}
